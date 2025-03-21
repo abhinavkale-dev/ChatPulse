0a90c5ecaddef2f084b32c03d61dcb92c275c51d
Abhi16016 <abhinavkale919913@gmail.com>
2025-03-21 22:52:26 +0530

    [fix]: Fixing redirects

--- client/app/(auth)/signin/page.tsx
+++ client/app/(auth)/signin/page.tsx
@@ -23,11 +23,7 @@ import { useRouter } from "next/navigation";
 import type { z } from "zod";
 
 export default function SigninPage() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-background px-4 py-12">
-      <Signin />
-    </div>
-  );
+  return <Signin />;
 }
 
 function Signin() {
