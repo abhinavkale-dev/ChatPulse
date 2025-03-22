8520210d84c4457ced9e27fe92c18f28e5dea958
Abhi16016 <abhinavkale919913@gmail.com>
2025-03-22 22:08:15 +0530

    [feat]: Added domain in CORS

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,7 +13,7 @@ const server = createServer(app)
 
 const io = new Server(server,{
     cors: {
-        origin: ["http://localhost:3000", "https://chat-pulse-gilt.vercel.app", "https://chatpulse.chat/"],
+        origin: ["http://localhost:3000", "https://chatpulse.chat/"],
         methods: ["GET", "POST"],
         credentials: true
     },
