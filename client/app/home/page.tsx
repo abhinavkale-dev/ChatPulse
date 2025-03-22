686123f6d714a5a122b06ed5aa0cd5ffde834e15
Abhi16016 <abhinavkale919913@gmail.com>
2025-03-23 00:17:30 +0530

    [fix]: Final touchups

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -3,7 +3,6 @@
 import { Button } from '@/components/ui/button'
 import { Card, CardContent } from '@/components/ui/card'
 import { MessageCircleMore, MoreVertical, Share, Trash2 } from 'lucide-react'
-import Link from 'next/link'
 import { useState, useEffect } from 'react'
 import {
   Dialog,
