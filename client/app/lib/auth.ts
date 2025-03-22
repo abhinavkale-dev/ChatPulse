206d54d745cba2079955622d8d4075849eefa7c9
Abhi16016 <abhinavkale919913@gmail.com>
2025-03-23 01:18:23 +0530

    [fix]: Fixed auth errors

--- client/app/lib/auth.ts
+++ client/app/lib/auth.ts
@@ -1,12 +1,10 @@
-import { PrismaClient } from "@prisma/client";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import bcrypt from "bcryptjs";
 import { signUpSchema, signInSchema } from "./zod";
 import { Session, User, Account, Profile } from "next-auth";
 import { JWT } from "next-auth/jwt";
-
-const prisma = new PrismaClient();
+import prisma from "./prisma";
 
 interface FormCredentials {
   email: string;
