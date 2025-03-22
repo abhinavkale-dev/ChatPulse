82d66e5795fa7b3f166752ef28838523d538eab8
Abhi16016 <abhinavkale919913@gmail.com>
2025-03-23 00:55:18 +0530

    [feat]: OpenGraph Added

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -31,7 +31,7 @@ export const metadata: Metadata = {
     siteName: "ChatPulse",
     images: [
       {
-        url: "https://imgur.com/a/GYOCRf8", 
+        url: "/banner.png", 
         width: 1200,
         height: 630,
         alt: "ChatPulse - A fast chat app",
@@ -44,7 +44,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "ChatPulse",
     description: "A fast chat app",
-    images: ["https://imgur.com/a/GYOCRf8"],
+    images: ["/banner.png"],
   },
 };
 
